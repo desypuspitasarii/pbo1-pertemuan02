6f91b6ee2d69c272bf95474e89e0711ee125039b
desypuspitasarii <desyeol003@gmail.com>
2020-05-03 00:38:56 +0100

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ====================");
+  System.out.println("NPM : 18630139");
+  System.out.println("Nama : Desy Puspita Sari");
+  System.out.println("==================================");
+ }
+}
