9f521be1a875699ae457732cd7f8b7e5281910b0
desypuspitasarii <desyeol003@gmail.com>
2020-05-03 04:35:28 +0100

    Compile & Run

--- InputOutput.java
+++ InputOutput.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class InputOutput
+public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
